9c41d1cd8a6b849ccd217ab7e12ba05ceea4147b
Thomas Ménès <64870819+OBud75@users.noreply.github.com>
2025-03-16 06:07:44 +0100

    review

--- exoscpp/user.cpp
+++ exoscpp/user.cpp
@@ -82,4 +82,32 @@ User &User::get(int id)
 
     file.close();
     throw std::runtime_error("User not found");
+    // C'est ca !
+    // Sans new, on crée un objet sur la pile (stack)
+    // et on retourne une copie de cet objet
+    // l'objet original est détruit à la fin de la fonction
+    // donc retourner un pointeur vers celui ci mène à un comportement indéfini
+
+    // Avec new, on crée un objet sur le tas (heap)
+    // et on retourne un pointeur vers cet objet
+    // l'objet ne sera détruit que si on appelle delete sur le pointeur.
+
+    // Ca rejoint l'idée du lifetime des objets qu'on retrouve en Rust.
+    // Pour aller plus loin si C++ et le fonctionnement de la mémoire vous interesse,
+    // regardez plus en profondeur les pointeurs uniques, partagés et faibles.
+    // Aussi, la fonction std::move qui permet de retourner un objet "original" sans copie.
 }
+
+
+
+// Si on veut aller un peu plus loin dans les tests:
+// tests fonctionnels:
+// tester la fonction save à part:
+// On créé un utilisateur, on le sauvegarde,
+// puis on le récupère mais sans passer par la fonction get et on compare.
+// Pareil pour get, l'idée serait d'avoir un utilsateur déjà créé dans la base (sans passer par save)
+
+// Puis vient ce que vous avez fait qui est plus un test d'intégration
+// où on teste l'ensemble des fonctionnalités ensemble.
+
+// Pour l'exercice en C, une fonction main qui prouve que tout fonctionne bien aurait été bienvenue.
